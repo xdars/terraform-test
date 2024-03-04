d63674991117f9a6bd032becbdce3c4599d1e446
alien <alien@eclipse.local>
2024-03-04 17:42:45 +0200

    add variables

--- variables.tf
+++ variables.tf
@@ -5,7 +5,19 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "workspace_to_environment_map" {
+  type = map(string)
+  default = {
+    dev = "dev"
+    prd = "prd"
+  }
+}
 
-variable "s3_name" {
-  default = "stourage-ultimately-smoothly-helping-dove"
+locals {
+  environment            = lookup(var.workspace_to_environment_map, terraform.workspace, "dev")
+  api_gateway_name       = "${local.environment}-api_gateway"
+  api_gateway_stage_name = "${local.environment}-api_gateway_stage"
+
+  make_file_lambda_name = "${local.environment}-make-file-lambda"
+  call_lambda_name      = "${local.environment}-call-lambda"
 }
